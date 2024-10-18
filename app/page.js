a85f9c028a3227f5164a97c28a9b9f457d5e365a
efobifrancis <francisefobi@gmail.com>
2024-10-18 09:48:31 +0100

    added attribution

--- app/page.js
+++ app/page.js
@@ -3,8 +3,13 @@ import Form from "@/components/Form";
 
 export default function Home() {
   return (
-    <div className=" font-PlusJakartaSans font-medium md:flex justify-center items-center md:h-screen md:bg-[hsl(199,81%,94%)] md:px-10 ">
+    <div className=" font-PlusJakartaSans font-medium md:flex md:flex-col justify-center items-center md:h-screen md:bg-[hsl(199,81%,94%)] md:px-10 ">
       <Form />
+
+      <div className="attribution mt-10">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+        Coded by <a href="#">Efobi Francis</a>.
+      </div>
     </div>
   );
 }
